ab6f1ab3615fdb05096b3872c8b81fabad9dc4d5
Vukašin Terzić <37403397+vukasinterzic@users.noreply.github.com>
2020-01-27 19:42:31 +0100

    Update Create-GitHubContributions.ps1

--- Create-GitHubContributions.ps1
+++ Create-GitHubContributions.ps1
@@ -1,2 +1,14 @@
 #This scripts let you fake your GitHub contributions history
 #TODO: Description
+
+#Define image, including guide on how to define it
+
+$MaximumContributions = ""
+
+$Diff = $MaximumContributions / 4
+
+$Level0 = 0
+$Level1 = $Diff
+$Level2 = $Diff * 2
+$Level3 = $Diff * 3
+$Level4 = $Diff * 4
